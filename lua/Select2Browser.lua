aeb35689ed3cb36c1cd543e7fede62add057b2d0
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2023-03-01 21:55:53 +0900

    update

--- lua/Select2Browser.lua
+++ lua/Select2Browser.lua
@@ -5,8 +5,14 @@ local M = {}
 M.config = {
   -- default config
   opt = "Hello!",
+  base_url = 'https://www.google.com/search?q={search_text}'
+  ,
+  base_cmd = 'open {open_url}'
 }
 
+
+
+vim.api.nvim_set_keymap("", "<Leader>g", ":Selected2Google<CR>", { noremap = true, silent = true })
 -- setup is the public method to setup your plugin
 M.setup = function(args)
   -- you can define your setup function here. Usually configurations can be merged, accepting outside params and
---
+++ lua/Select2Browser/module.lua
@@ -0,0 +1,31 @@
+-- module represents a lua module for the plugin
+local M = {}
+M.my_first_function = function()
+  return "hello world!"
+end
+
+M.get_visual_selection = function()
+  local s_start = vim.fn.getpos("'<")
+  local s_end = vim.fn.getpos("'>")
+  local n_lines = math.abs(s_end[2] - s_start[2]) + 1
+  local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
+  lines[1] = string.sub(lines[1], s_start[3], -1)
+
+  if n_lines == 1 then
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
+  else
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
+  end
+
+  return table.concat(lines, '\n')
+end
+
+M.search_selection_in_google = function(text, base_url, base_cmd)
+  local selected_text     = text
+  local new_url           = base_url:gsub("{search_text}", selected_text)
+  local open_cmd          = base_cmd:gsub("{open_url}", '`' .. new_url .. '`')
+  local remove_line_break = open_cmd:gsub("\n", " ")
+  vim.api.nvim_command("! " .. remove_line_break)
+end
+
+return M
